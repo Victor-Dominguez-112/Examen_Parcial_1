a5de53b565c8cf2b1148d3ac530c9519b80a8a35
Victor Ricardo Dominguez Cruz <165311537+Victor-Dominguez-112@users.noreply.github.com>
2025-10-01 23:26:13 -0600

    Avance 1 ejercicio 1

---
+++ ExamenPOO/Ejercicio1_Estructuras.cpp
@@ -0,0 +1,41 @@
+//Ejercicio Estructuras : 35 puntos.
+//1) Hacer una estructura(struct) llamada VectorDeValores.
+//1.1) Internamente tiene dos variables, uno es un puntero a float, y la otra es un int para el tamaño del array.
+//1.2) Hacer un constructor que reciba como parámetros un array de floats y un int del tamaño del array de floats y los asigne a las variables internas.
+//1.3) Hacer un destructor que libere la memoria dinámica solicitada, e imprima cuántos elementos liberó.
+//1.4) Hacer un constructor por defecto, que ponga el tamaño del array en 2, y pida la memoria dinámica correspondiente, y después inicialice esos valores en 0.
+
+#include "Ejercicio1_Estructuras.h"
+#include <iostream>
+using namespace std;
+
+//Constructor con parametros
+VectorDeValores::VectorDeValores(float Myarray[], int tamano)
+{
+	TamanoArray = tamano;
+	PuntFloat = new float[TamanoArray]; //pediremos memoria para la variable TamanoArray
+	for (int i = 0; i < TamanoArray; i++)
+	{
+		PuntFloat[i] = Myarray[i]; //Copiaremos los valores
+	}
+	cout << "Creacion de un vector con " << TamanoArray << " elementos" << endl;
+}
+
+//Destructor
+VectorDeValores::~VectorDeValores()
+{
+	delete[] PuntFloat; //Liberammos la memoria ocupada o eliminar
+	cout << "Se elimino un vector con " << TamanoArray << " elementos" << endl;
+}
+
+//Contructor por defecto
+VectorDeValores::VectorDeValores()
+{
+	TamanoArray = 2; //Damos un tamaño de 2
+	PuntFloat = new float[TamanoArray]; //pedimos la memoria
+	for (int i = 0; i < TamanoArray; i++)
+	{
+		PuntFloat[i] = 0; //Iniciamos en 0
+	}
+	cout << "Creacion de un vector con tamaño de " << TamanoArray << endl;
+}
---
+++ ExamenPOO/Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+#include "Ejercicio1_Estructuras.h"
+
+int main()
+{
+	//Ejercicio 1		Ejercicio 1		Ejercicio 1 
+	//Destructor por defecto
+	VectorDeValores Uno;
+	cout << "Vector De Valores numero Uno: ";
+	for (int i = 0; i < Uno.TamanoArray; i++)
+	{
+		cout << Uno.PuntFloat[i] << " ";
+	}
+	cout << endl;
+
+	// Destructor con parámetros
+	float array[3] = { 1.5,2.5,3.5 };
+	VectorDeValores Dos;
+	cout << "Vector De Valores numero Dos: ";
+	for (int i = 0; i < Dos.TamanoArray; i++)
+	{
+		cout << Dos.PuntFloat[i] << " ";
+	}
+	cout << endl;
+	//Ejercicio 1		Ejercicio 1		Ejercicio 1 
+
+
+
+	//Ejercicio 2		Ejercicio 2		Ejercicio 2 
+	//Ejercicio 2		Ejercicio 2		Ejercicio 2 
+
+
+
+	//Ejercicio 3		Ejercicio 3		Ejercicio 3 
+	//Ejercicio 3		Ejercicio 3		Ejercicio 3
+
+	return 0;
+}
